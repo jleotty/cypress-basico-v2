f48b60846abb946fc217eefae1d164392044a58b
jleotty <julianleotty@gmail.com>
2024-07-15 14:36:19 -0300

    commit

--- cypress/integration/exercicio_22.spec.js
+++ cypress/integration/exercicio_22.spec.js
@@ -4,9 +4,14 @@ describe('exibe mensagem de erro ao submeter o formulário com um email com form
     beforeEach(function(){ //beforeeach é a mesma coisa que: antes de realizar o teste, executar seguinte comando
         cy.visit('./src/index.html');
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+    });
 
-        it('T')
+    it('acessa a página da política de privacidade removendo o target e então clicando no link,', function() {
+      
+        cy.get('#privacy > a').should('have.attr', 'target', '_blank') // verificando se o link de privacidade que abriria uma nova aba se encontra, "have.attr" serve para validar atributo
+       
 
-
+        
+        
     });
 });
